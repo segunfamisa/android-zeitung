ce25826b207943d4756c3aed0f54aabc102e94e8
Segun Famisa <segunfamisa@gmail.com>
2018-11-14 22:00:46 +0100

    Add entity for article

---
+++ core/src/main/java/com/segunfamisa/zeitung/core/entities/Article.kt
@@ -0,0 +1,14 @@
+package com.segunfamisa.zeitung.core.entities
+
+import java.util.Date
+
+data class Article(
+    val source: Source,
+    val author: String,
+    val title: String,
+    val description: String,
+    val url: String,
+    val imageUrl: String,
+    val publishedAt: Date,
+    val content: String
+)
