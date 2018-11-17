657bd029aec9d133ee57bee1df48a92fd9e9c29d
Segun Famisa <segunfamisa@gmail.com>
2018-11-17 20:46:12 +0100

    Add `@Inject` annotations

--- data/src/main/java/com/segunfamisa/zeitung/data/remote/ApiKeyProviderImpl.kt
+++ data/src/main/java/com/segunfamisa/zeitung/data/remote/ApiKeyProviderImpl.kt
@@ -1,8 +1,9 @@
 package com.segunfamisa.zeitung.data.remote
 
 import com.segunfamisa.zeitung.data.BuildConfig
+import javax.inject.Inject
 
-class ApiKeyProviderImpl : ApiKeyProvider {
+class ApiKeyProviderImpl @Inject constructor() : ApiKeyProvider {
 
     override fun getApiKey(): String {
         return BuildConfig.ApiKey
