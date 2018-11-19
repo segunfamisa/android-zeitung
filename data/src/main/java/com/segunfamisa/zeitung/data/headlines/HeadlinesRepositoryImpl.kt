27dd32e48bb38fcc0e775f3df8a8d05459b92c25
Segun Famisa <segunfamisa@gmail.com>
2018-11-19 22:24:46 +0100

    Implement headlines repository
    * Current implementation simply fetches data using the remote data
    source

---
+++ data/src/main/java/com/segunfamisa/zeitung/data/headlines/HeadlinesRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.segunfamisa.zeitung.data.headlines
+
+import arrow.core.Either
+import com.segunfamisa.zeitung.core.entities.Article
+import com.segunfamisa.zeitung.domain.common.Error
+import com.segunfamisa.zeitung.domain.common.Result
+import com.segunfamisa.zeitung.domain.headlines.HeadlinesRepository
+import javax.inject.Inject
+
+internal class HeadlinesRepositoryImpl @Inject constructor(
+    private val remoteSource: HeadlinesSource
+) : HeadlinesRepository {
+    override suspend fun getHeadlines(
+        category: String,
+        country: String,
+        sources: String
+    ): Either<Error, Result<List<Article>>> {
+        return remoteSource.getHeadlines(category = category, country = category, sources = sources)
+            .map {
+                Result(data = it)
+            }
+    }
+}
