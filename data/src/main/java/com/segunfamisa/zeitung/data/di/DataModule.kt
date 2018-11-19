af11c6ca6fb336ab9aebe88bce0b13df2510dea7
Segun Famisa <segunfamisa@gmail.com>
2018-11-19 23:19:40 +0100

    Register dependencies in DataModule

---
+++ data/src/main/java/com/segunfamisa/zeitung/data/di/DataModule.kt
@@ -0,0 +1,47 @@
+package com.segunfamisa.zeitung.data.di
+
+import com.segunfamisa.zeitung.data.di.qualifiers.DataSource
+import com.segunfamisa.zeitung.data.headlines.HeadlinesRepositoryImpl
+import com.segunfamisa.zeitung.data.headlines.HeadlinesSource
+import com.segunfamisa.zeitung.data.headlines.RemoteHeadlinesSource
+import com.segunfamisa.zeitung.data.remote.ApiKeyProvider
+import com.segunfamisa.zeitung.data.remote.ApiKeyProviderImpl
+import com.segunfamisa.zeitung.data.remote.ApiService
+import com.segunfamisa.zeitung.data.remote.ApiServiceCreator
+import com.segunfamisa.zeitung.data.remote.UrlProvider
+import com.segunfamisa.zeitung.data.remote.UrlProviderImpl
+import com.segunfamisa.zeitung.domain.headlines.HeadlinesRepository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+
+@Module
+abstract class DataModule {
+
+    @Module
+    companion object {
+
+        @Provides
+        @JvmStatic
+        internal fun provideApiService(apiServiceCreator: ApiServiceCreator): ApiService {
+            return apiServiceCreator.createService()
+        }
+    }
+
+    // region Remote data
+
+    @Binds
+    internal abstract fun bindHeadlineRepository(headlineRepository: HeadlinesRepositoryImpl): HeadlinesRepository
+
+    @Binds
+    internal abstract fun bindApiKeyProvider(apiKeyProvider: ApiKeyProviderImpl): ApiKeyProvider
+
+    @Binds
+    internal abstract fun bindUrlProvider(urlProvider: UrlProviderImpl): UrlProvider
+
+    @Binds
+    @DataSource(type = "remote")
+    internal abstract fun bindRemoteHeadlineSource(remoteSource: RemoteHeadlinesSource): HeadlinesSource
+
+    // endregion
+}
