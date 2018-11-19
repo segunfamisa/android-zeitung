399a0634d93cdc5570bae5cc03c24616ed961fb4
Segun Famisa <segunfamisa@gmail.com>
2018-11-19 23:18:34 +0100

    Add data source qualifier to headlines repository implementation

--- data/src/main/java/com/segunfamisa/zeitung/data/headlines/HeadlinesRepositoryImpl.kt
+++ data/src/main/java/com/segunfamisa/zeitung/data/headlines/HeadlinesRepositoryImpl.kt
@@ -2,13 +2,14 @@ package com.segunfamisa.zeitung.data.headlines
 
 import arrow.core.Either
 import com.segunfamisa.zeitung.core.entities.Article
+import com.segunfamisa.zeitung.data.di.qualifiers.DataSource
 import com.segunfamisa.zeitung.domain.common.Error
 import com.segunfamisa.zeitung.domain.common.Result
 import com.segunfamisa.zeitung.domain.headlines.HeadlinesRepository
 import javax.inject.Inject
 
 internal class HeadlinesRepositoryImpl @Inject constructor(
-    private val remoteSource: HeadlinesSource
+    @DataSource(type = "remote") private val remoteSource: HeadlinesSource
 ) : HeadlinesRepository {
     override suspend fun getHeadlines(
         category: String,
