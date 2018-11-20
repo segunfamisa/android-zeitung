bd7855c8df98c73c6f8d54b10f57e089c4e535fa
Segun Famisa <segunfamisa@gmail.com>
2018-11-20 23:03:14 +0100

    Add news sources data source interface

---
+++ data/src/main/java/com/segunfamisa/zeitung/data/newssources/NewsSourcesDataSource.kt
@@ -0,0 +1,13 @@
+package com.segunfamisa.zeitung.data.newssources
+
+import arrow.core.Either
+import com.segunfamisa.zeitung.core.entities.Source
+import com.segunfamisa.zeitung.domain.common.Error
+
+/**
+ * Interface to get news sources
+ */
+internal interface NewsSourcesDataSource {
+
+    suspend fun getNewsSources(category: String, language: String, country: String): Either<Error, List<Source>>
+}
