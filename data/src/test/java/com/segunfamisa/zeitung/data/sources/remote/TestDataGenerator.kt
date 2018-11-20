6c3e48d6f83cfda099bdf99df563f6fbd9ca81e6
Segun Famisa <segunfamisa@gmail.com>
2018-11-20 23:03:53 +0100

    Add method to create sources

--- data/src/test/java/com/segunfamisa/zeitung/data/sources/remote/TestDataGenerator.kt
+++ data/src/test/java/com/segunfamisa/zeitung/data/sources/remote/TestDataGenerator.kt
@@ -1,6 +1,7 @@
 package com.segunfamisa.zeitung.data.sources.remote
 
 import com.segunfamisa.zeitung.data.sources.remote.entities.Article
+import com.segunfamisa.zeitung.data.sources.remote.entities.Source
 import com.segunfamisa.zeitung.data.sources.remote.entities.SourceMinimal
 import java.util.Date
 
@@ -27,5 +28,23 @@ internal object TestDataGenerator {
         }
 
         return articles
+    }
+
+    fun createSources(count: Int): List<Source> {
+        val sources = mutableListOf<Source>()
+        repeat(count) {
+            sources.add(
+                Source(
+                    id = "id $it",
+                    language = "language $it",
+                    country = "country $it",
+                    category = "category $it",
+                    description = "description $it",
+                    url = "url $it",
+                    name = "name $it"
+                )
+            )
+        }
+        return sources
     }
 }
