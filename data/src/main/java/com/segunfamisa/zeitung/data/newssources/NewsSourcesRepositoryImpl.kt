fe116d7c7322989f117790822b100ac50d0f466a
Segun Famisa <segunfamisa@gmail.com>
2018-11-20 23:12:43 +0100

    Add qualifier to remote source

--- data/src/main/java/com/segunfamisa/zeitung/data/newssources/NewsSourcesRepositoryImpl.kt
+++ data/src/main/java/com/segunfamisa/zeitung/data/newssources/NewsSourcesRepositoryImpl.kt
@@ -2,13 +2,14 @@ package com.segunfamisa.zeitung.data.newssources
 
 import arrow.core.Either
 import com.segunfamisa.zeitung.core.entities.Source
+import com.segunfamisa.zeitung.data.di.qualifiers.DataSource
 import com.segunfamisa.zeitung.domain.common.Error
 import com.segunfamisa.zeitung.domain.common.Result
 import com.segunfamisa.zeitung.domain.newssources.NewsSourcesRepository
 import javax.inject.Inject
 
 internal class NewsSourcesRepositoryImpl @Inject constructor(
-    private val remoteSource: NewsSourcesDataSource
+    @DataSource("remote") private val remoteSource: NewsSourcesDataSource
 ) : NewsSourcesRepository {
 
     override suspend fun getNewsSources(
