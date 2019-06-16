5e7b052203595e0bfada412bccdcfb38bdd69b19
Segun Famisa <segunfamisa@gmail.com>
2019-06-16 15:20:58 +0200

    Add Ui representation of an article

---
+++ app/src/main/java/com/segunfamisa/zeitung/ui/news/UiArticleItem.kt
@@ -0,0 +1,9 @@
+package com.segunfamisa.zeitung.ui.news
+
+data class UiArticleItem(
+    val source: String,
+    val title: String,
+    val imageUrl: String,
+    val description: String,
+    val date: String
+)
