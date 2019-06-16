0fcf4f4b3e991ec50b1c28339a9b96efdfe42764
Segun Famisa <segunfamisa@gmail.com>
2019-06-16 16:17:43 +0200

    Add view model event object
    This makes use of the event pattern, so that events are not re-consumed
    on configuration changes or activity restart.

---
+++ app/src/main/java/com/segunfamisa/zeitung/util/viewmodel/Event.kt
@@ -0,0 +1,18 @@
+package com.segunfamisa.zeitung.util.viewmodel
+
+class Event<T>(private val content: T) {
+    var hasBeenHandled = false
+        private set
+
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    fun peekContent(): T = content
+}
