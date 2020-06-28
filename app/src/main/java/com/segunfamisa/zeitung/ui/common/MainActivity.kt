7ce926059f388010263c121e171848f11a0a1ab1
Segun Famisa <segun.famisa@soundcloud.com>
2020-06-28 11:15:43 +0200

    Select news section by default

--- app/src/main/java/com/segunfamisa/zeitung/ui/common/MainActivity.kt
+++ app/src/main/java/com/segunfamisa/zeitung/ui/common/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.segunfamisa.zeitung.ui.common
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.segunfamisa.zeitung.R
 import com.segunfamisa.zeitung.databinding.ActivityMainBinding
+import com.segunfamisa.zeitung.headlines.HeadlinesFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +17,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.bottomNav.setOnNavigationItemSelectedListener {
+            setBottomNavSelection(it.itemId)
+        }
+        binding.bottomNav.selectedItemId = R.id.menu_news
+    }
+
+    private fun setBottomNavSelection(@IdRes itemId: Int): Boolean {
+        return when (itemId) {
+            R.id.menu_news -> {
+                addFragment(HeadlinesFragment::class.java.name) {
+                    HeadlinesFragment()
+                }
+                true
+            }
+            else -> {
+                Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show()
+                false
+            }
+        }
+    }
+
+    private inline fun addFragment(tag: String, factory: () -> Fragment) {
+        val frag = supportFragmentManager.findFragmentByTag(tag) ?: factory.invoke()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, frag, tag)
+            .commit()
     }
 }
