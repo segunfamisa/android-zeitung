fe0b08150f67167af1d3249b687f32d669887554
Segun Famisa <segun.famisa@soundcloud.com>
2020-07-03 11:06:16 +0200

    Restructure dependencies.
    Containers providing modules will be added into the MainActivityModule

--- app/src/main/java/com/segunfamisa/zeitung/di/AppModule.kt
+++ app/src/main/java/com/segunfamisa/zeitung/di/AppModule.kt
@@ -1,13 +1,10 @@
 package com.segunfamisa.zeitung.di
 
 import com.segunfamisa.zeitung.data.di.DataModule
-import com.segunfamisa.zeitung.ui.news.NewsModule
-import dagger.Binds
 import dagger.Module
 
-@Module(includes = [DataModule::class, NewsModule::class])
+@Module(includes = [DataModule::class])
 abstract class AppModule {
 
-    @Binds
-    abstract fun bindAppContainer(appContainerImpl: AppContainerImpl): AppContainer
+
 }
