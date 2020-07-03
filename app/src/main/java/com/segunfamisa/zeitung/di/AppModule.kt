aea19dcb9e44564c894264cc5f291cc76ba5f891
Segun Famisa <segun.famisa@soundcloud.com>
2020-07-03 11:14:32 +0200

    Create coroutine dispatcher provider

--- app/src/main/java/com/segunfamisa/zeitung/di/AppModule.kt
+++ app/src/main/java/com/segunfamisa/zeitung/di/AppModule.kt
@@ -1,10 +1,14 @@
 package com.segunfamisa.zeitung.di
 
 import com.segunfamisa.zeitung.data.di.DataModule
+import com.segunfamisa.zeitung.util.DefaultDispatcherProvider
+import com.segunfamisa.zeitung.util.DispatcherProvider
+import dagger.Binds
 import dagger.Module
 
 @Module(includes = [DataModule::class])
 abstract class AppModule {
 
-
+    @Binds
+    abstract fun bindDispatcherProvider(provider: DefaultDispatcherProvider): DispatcherProvider
 }
---
+++ app/src/main/java/com/segunfamisa/zeitung/util/DispatcherProvider.kt
@@ -0,0 +1,18 @@
+package com.segunfamisa.zeitung.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+interface DispatcherProvider {
+    val io: CoroutineDispatcher
+    val main: CoroutineDispatcher
+}
+
+class DefaultDispatcherProvider @Inject constructor() : DispatcherProvider {
+    override val io: CoroutineDispatcher
+        get() = Dispatchers.IO
+    override val main: CoroutineDispatcher
+        get() = Dispatchers.Main
+
+}
