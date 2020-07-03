b89202781749e185d78a3cec87ae500b2ae2f114
Segun Famisa <segun.famisa@soundcloud.com>
2020-07-03 11:38:29 +0200

    Add function to get headlines

--- app/src/main/java/com/segunfamisa/zeitung/ui/news/NewsViewModel.kt
+++ app/src/main/java/com/segunfamisa/zeitung/ui/news/NewsViewModel.kt
@@ -1,12 +1,33 @@
 package com.segunfamisa.zeitung.ui.news
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.segunfamisa.zeitung.domain.headlines.GetHeadlinesUseCase
+import com.segunfamisa.zeitung.domain.headlines.HeadlineQueryParam
+import com.segunfamisa.zeitung.util.DispatcherProvider
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class NewsViewModel @Inject constructor(
-    private val getHeadlinesUseCase: GetHeadlinesUseCase
-): ViewModel() {
+    private val getHeadlinesUseCase: GetHeadlinesUseCase,
+    private val dispatcherProvider: DispatcherProvider
+) : ViewModel() {
 
+    fun fetchHeadlines() {
+        viewModelScope.launch(dispatcherProvider.io) {
+            val params = HeadlineQueryParam(category = "technology", country = "de")
+            val headlines = getHeadlinesUseCase.invoke(params)
+            headlines.fold({
+                Log.e(LOG_TAG, it.message)
+            }, {
+                Log.d(LOG_TAG, it.toString())
+                Unit
+            })
+        }
+    }
 
+    private companion object {
+        const val LOG_TAG = "NewsViewModel"
+    }
 }
