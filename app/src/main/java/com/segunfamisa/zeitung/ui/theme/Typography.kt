36f899224dee461b07904b333697a79f8b529ce1
Segun Famisa <segun.famisa@soundcloud.com>
2020-09-01 18:09:50 +0200

    Add custom Raleway fonts to the app

---
+++ app/src/main/java/com/segunfamisa/zeitung/ui/theme/Typography.kt
@@ -0,0 +1,89 @@
+package com.segunfamisa.zeitung.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.font
+import androidx.compose.ui.text.font.fontFamily
+import androidx.compose.ui.unit.sp
+import com.segunfamisa.zeitung.R
+
+private val raleWayFontFamily = fontFamily(
+    font(R.font.raleway_light, FontWeight.Light),
+    font(R.font.raleway_regular, FontWeight.Normal),
+    font(R.font.raleway_medium, FontWeight.Medium),
+
+    font(R.font.raleway_light_italic, FontWeight.Light, FontStyle.Italic),
+    font(R.font.raleway_regular_italic, FontWeight.Normal, FontStyle.Italic),
+    font(R.font.raleway_medium_italic, FontWeight.Medium, FontStyle.Italic)
+)
+
+fun zeitungTypography() = Typography(
+    defaultFontFamily = raleWayFontFamily,
+    h1 = TextStyle(
+        fontWeight = FontWeight.Light,
+        fontSize = 96.sp,
+        letterSpacing = (-1.5).sp
+    ),
+    h2 = TextStyle(
+        fontWeight = FontWeight.Light,
+        fontSize = 60.sp,
+        letterSpacing = (-0.5).sp
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 48.sp,
+        letterSpacing = 0.sp
+    ),
+    h4 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 34.sp,
+        letterSpacing = 0.25.sp
+    ),
+    h5 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    h6 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle1 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle2 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        letterSpacing = 0.1.sp
+    ),
+    body1 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    body2 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.25.sp
+    ),
+    button = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        letterSpacing = 1.25.sp
+    ),
+    caption = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        letterSpacing = 0.4.sp
+    ),
+    overline = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
+        letterSpacing = 1.5.sp
+    )
+)
