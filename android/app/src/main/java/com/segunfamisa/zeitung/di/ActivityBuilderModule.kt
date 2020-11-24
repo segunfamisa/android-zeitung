eb686768ce18753a8eb50d79d0622df5c4ef6a76
Segun Famisa <segun.famisa@soundcloud.com>
2020-11-24 12:08:16 +0100

    Remove old container pattern in favour of lazy

--- android/app/src/main/java/com/segunfamisa/zeitung/di/ActivityBuilderModule.kt
+++ android/app/src/main/java/com/segunfamisa/zeitung/di/ActivityBuilderModule.kt
@@ -1,13 +1,12 @@
 package com.segunfamisa.zeitung.di
 
 import com.segunfamisa.zeitung.ui.common.MainActivity
-import com.segunfamisa.zeitung.ui.common.MainActivityModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBuilderModule {
 
-    @ContributesAndroidInjector(modules = [MainActivityModule::class])
+    @ContributesAndroidInjector
     abstract fun contributeMainActivity(): MainActivity
 }
