3759b9feb61464acc6b7df43cb5d0a58fe06df9b
Segun Famisa <segun.famisa@soundcloud.com>
2020-11-26 06:10:43 +0100

    Add flow abstract use case

--- shared/domain/src/main/java/com/segunfamisa/zeitung/domain/common/UseCase.kt
+++ shared/domain/src/main/java/com/segunfamisa/zeitung/domain/common/UseCase.kt
@@ -1,8 +1,14 @@
 package com.segunfamisa.zeitung.domain.common
 
 import arrow.core.Either
+import kotlinx.coroutines.flow.Flow
 
 abstract class UseCase<in Param, out Output> {
 
     abstract suspend operator fun invoke(param: Param): Either<Error, Output>
+}
+
+abstract class FlowUseCase<in Param, out Result> {
+
+    abstract fun execute(param: Param): Flow<Either<Error, Result>>
 }
