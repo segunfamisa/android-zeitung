21da195d3a71aae4ec9834cc3c9c3830ad97c419
Segun Famisa <segun.famisa@soundcloud.com>
2020-11-29 10:19:05 +0100

    Remove existing manual solution to navigation in favour of Jetpack Compose navigation

--- android/app/src/main/java/com/segunfamisa/zeitung/ui/common/MainActivity.kt
+++ android/app/src/main/java/com/segunfamisa/zeitung/ui/common/MainActivity.kt
@@ -1,31 +1,48 @@
 package com.segunfamisa.zeitung.ui.common
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.ViewModelLazy
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
 import com.segunfamisa.zeitung.R
 import com.segunfamisa.zeitung.common.theme.ZeitungTheme
 import com.segunfamisa.zeitung.news.NewsContent
 import com.segunfamisa.zeitung.news.NewsViewModel
+import com.segunfamisa.zeitung.onboarding.OnboardingContent
+import com.segunfamisa.zeitung.onboarding.OnboardingViewModel
 import com.segunfamisa.zeitung.util.viewmodel.ViewModelFactory
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var viewModelFactory: ViewModelFactory<NewsViewModel>
+    lateinit var newsViewModelFactory: ViewModelFactory<NewsViewModel>
+
+    @Inject
+    lateinit var onboardingViewModelFactory: ViewModelFactory<OnboardingViewModel>
 
     private val newsViewModelLazy: Lazy<NewsViewModel> = ViewModelLazy(
         viewModelClass = NewsViewModel::class,
         storeProducer = { viewModelStore },
-        factoryProducer = { viewModelFactory }
+        factoryProducer = { newsViewModelFactory }
+    )
+
+    private val onboardingViewModelLazy: Lazy<OnboardingViewModel> = ViewModelLazy(
+        viewModelClass = OnboardingViewModel::class,
+        storeProducer = { viewModelStore },
+        factoryProducer = { onboardingViewModelFactory }
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,54 +57,67 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     private fun App() {
-        val navBarState = NavBarState(listOf(NavItem.News, NavItem.Explore, NavItem.Bookmarks))
-        val screenState = ScreenState(Screen.News)
-
-        val navigator = object : Navigator {
-            override fun navigateTo(screen: Screen) {
-                screenState.currentScreen = screen
+        val onboardingNavController = rememberNavController()
+        NavHost(navController = onboardingNavController, startDestination = Routes.Onboarding) {
+            composable(Routes.Onboarding) {
+                OnboardingContent(
+                    onboardingViewModel = onboardingViewModelLazy,
+                    onContinue = {
+                        onboardingViewModelLazy.value.onContinueClicked(token = it)
+                    },
+                    onApiTokenChange = {
+                        onboardingViewModelLazy.value.onApiTokenChange(token = it)
+                    },
+                )
+                val observeState = onboardingViewModelLazy.value.continueToApp.observeAsState()
+                if (observeState.value == true) {
+                    onboardingNavController.navigate(route = Routes.Main)
+                }
+            }
+            composable(Routes.Main) {
+                Main()
             }
         }
+    }
 
+    @Composable
+    private fun Main() {
+        val navController = rememberNavController()
+        val scaffoldState = rememberScaffoldState()
         Scaffold(
+            scaffoldState = scaffoldState,
             topBar = {
                 TopAppBar(
                     title = { Text(stringResource(R.string.app_name)) }
                 )
             },
-            bodyContent = {
-                when (screenState.currentScreen) {
-                    is Screen.News -> {
-                        NewsContent(
-                            newsViewModel = newsViewModelLazy,
-                            onItemClicked = {
-                                // Handle click listener
-                            }
-                        )
-                        newsViewModelLazy.value.fetchHeadlines()
-                    }
-                    else -> Unit
-                }
-
-            },
             bottomBar = {
                 BottomNavBar(
-                    state = navBarState,
-                    onItemSelected = {
-                        setBottomNavSelection(it, navigator)
-                    }
+                    navController = navController,
+                    items = listOf(NavItem.News, NavItem.Explore, NavItem.Bookmarks)
                 )
             }
-        )
+        ) {
+            NavHost(navController = navController, startDestination = Routes.News) {
+                composable(Routes.Explore) {
+                    Text(text = "Explore")
+                }
+                composable(Routes.Bookmarks) {
+                    Text(text = "Bookmarks")
+                }
+                composable(Routes.News) {
+                    NewsContent(
+                        newsViewModel = newsViewModelLazy,
+                        onItemClicked = {
+                            // Handle click listener
+                        }
+                    )
+                    LaunchedEffect(Routes.News) {
+                        newsViewModelLazy.value.fetchHeadlines()
+                    }
+                }
+            }
+        }
     }
 
-    private fun setBottomNavSelection(navItem: NavItem, navigator: Navigator): Boolean {
-        // navigate to corresponding screen
-        when (navItem) {
-            is NavItem.News -> navigator.navigateTo(Screen.News)
-            else -> Toast.makeText(this, "Not yet implemented ${navItem.index}", Toast.LENGTH_SHORT)
-                .show()
-        }
-        return true
-    }
 }
