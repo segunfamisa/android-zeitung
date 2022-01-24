8732b8d710f083ca8839afd0c52f279af7189a82
Segun Famisa <segunfamisa@gmail.com>
2022-01-25 00:03:40 +0100

    Add implementation of custom button nav bar

--- android/app/src/main/java/com/segunfamisa/zeitung/ui/common/BottomNavBar.kt
+++ android/app/src/main/java/com/segunfamisa/zeitung/ui/common/BottomNavBar.kt
@@ -2,22 +2,29 @@ package com.segunfamisa.zeitung.ui.common
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.segunfamisa.zeitung.R
+import com.segunfamisa.zeitung.common.theme.ZeitungTheme
 import com.segunfamisa.zeitung.common.theme.colors
 
 sealed class NavItem(
@@ -68,6 +75,64 @@ fun BottomNavBar(
 }
 
 @Composable
+fun BottomNav(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    content: @Composable () -> Unit
+) {
+    Surface(elevation = 4.dp) {
+        Row(modifier = modifier
+            .background(backgroundColor)
+            .padding(8.dp)
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+private fun BottomNavItem(
+    modifier: Modifier = Modifier,
+    isSelected: Boolean = false,
+    item: NavItem,
+    onClick: () -> Unit
+) {
+    val backgroundColor = if (isSelected) MaterialTheme.colors.primary.copy(alpha = 0.1f) else Color.Transparent
+    val contentColor = if (isSelected) MaterialTheme.colors.primaryVariant else MaterialTheme.colors.onBackground
+
+    Box(modifier = modifier
+        .then(
+            Modifier
+                .clip(CircleShape)
+                .background(backgroundColor)
+                .clickable { onClick() }
+        )) {
+        Row(modifier = Modifier.padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+
+            Icon(
+                painter = painterResource(id = item.icon),
+                contentDescription = null,
+                modifier = Modifier,
+                tint = contentColor
+            )
+
+            AnimatedVisibility(visible = isSelected) {
+                Text(
+                    text = stringResource(id = item.title),
+                    color = contentColor
+                )
+            }
+        }
+    }
+}
+
+@Composable
 private fun BottomNavIcon(
     navItem: NavItem
 ) {
@@ -87,4 +152,56 @@ private fun BottomNavText(
         text = stringResource(id = navItem.title),
         fontWeight = if (isSelected) FontWeight.Bold else null
     )
+}
+
+@Preview
+@Composable
+private fun BottomNavItemPreview() {
+    ZeitungTheme(darkTheme = false) {
+        val isSelected = true
+        val navItem = NavItem.News
+        BottomNavItem(
+            isSelected = isSelected,
+            onClick = {
+
+            },
+            item = navItem
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun BottomNavItemPreviewDarkMode() {
+    ZeitungTheme(darkTheme = true) {
+        val isSelected = true
+        val navItem = NavItem.News
+        BottomNavItem(
+            isSelected = isSelected,
+            onClick = {
+
+            },
+            item = navItem
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun BottomNavPreview() {
+    ZeitungTheme {
+        val items = listOf(NavItem.News, NavItem.Explore, NavItem.Bookmarks)
+        BottomNav() {
+            items.forEachIndexed { index, navItem ->
+                val isSelected = index == 0
+                BottomNavItem(
+                    isSelected = isSelected,
+                    onClick = {
+
+                    },
+                    item = navItem
+                )
+            }
+        }
+    }
 }
