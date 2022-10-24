931a92712076d5f7dade4c5470948085e4d5cd99
Segun Famisa <segunfamisa@gmail.com>
2022-10-25 00:04:53 +0200

    Use version catalogs for project plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,14 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.extra["kotlin_version"]}")
+        classpath(libs.android.gradleplugin)
+        classpath(libs.kotlin.gradleplugin)
     }
 }
 
 plugins {
-    id("io.gitlab.arturbosch.detekt").version("1.22.0-RC2")
-    id("org.jlleitschuh.gradle.ktlint").version("9.2.1")
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.ktlint)
 }
 
 tasks.register("clean", Delete::class) {
