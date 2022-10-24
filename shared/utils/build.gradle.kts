e9b8444688f069e1e873113caee5996d2da7c7ad
Segun Famisa <segunfamisa@gmail.com>
2022-10-25 01:22:22 +0200

    Migrate `:shared:utils` to .kts and version catalogs

---
+++ shared/utils/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("java-library")
+    id("kotlin")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+dependencies {
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlin.coroutines.core)
+
+    implementation(libs.dagger.core)
+}
