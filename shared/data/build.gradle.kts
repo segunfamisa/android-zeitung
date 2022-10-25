85b293d13b30159fd52d5887a726770843addd15
Segun Famisa <segunfamisa@gmail.com>
2022-10-25 20:35:36 +0200

    Migrate `:shared:data` to .kts and version catalogs

---
+++ shared/data/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm")
+    kotlin("kapt")
+    id("idea")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation(project(":shared:core"))
+    implementation(project(":shared:domain"))
+
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlin.coroutines.core)
+
+    implementation(libs.arrow)
+
+    implementation(libs.dagger.core)
+    kapt(libs.dagger.compiler)
+
+    testImplementation(libs.test.junit)
+    testImplementation(libs.test.mockito)
+}
