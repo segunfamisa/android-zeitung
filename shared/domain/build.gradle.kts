bf2d37e8afaf7e9af5c3d9e1840bb8a0fa5a607f
Segun Famisa <segunfamisa@gmail.com>
2022-10-25 20:29:26 +0200

    Migrate `:shared:domain` to .kts and version catalogs

---
+++ shared/domain/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java-library")
+    id("kotlin")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+dependencies {
+    implementation(project(":shared:core"))
+
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlin.coroutines.core)
+    implementation(libs.arrow)
+    implementation(libs.javax.inject)
+
+    testImplementation(libs.test.junit)
+    testImplementation(libs.test.mockito)
+}
