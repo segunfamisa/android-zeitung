5bfb280803e60ba7ebbdbae9491d13c7cfa5ad4d
Segun Famisa <segunfamisa@gmail.com>
2023-09-16 12:57:23 +0200

    Add sources view model

---
+++ android/sources/src/main/java/com/segunfamisa/zeitung/sources/SourcesViewModel.kt
@@ -0,0 +1,54 @@
+package com.segunfamisa.zeitung.sources
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.segunfamisa.zeitung.sources.internal.GetNewsSourcesUseCase
+import com.segunfamisa.zeitung.sources.internal.SourcesQueryParam
+import com.segunfamisa.zeitung.sources.internal.UiSourceMapper
+import com.segunfamisa.zeitung.sources.internal.UiState
+import com.segunfamisa.zeitung.sources.internal.UserPreferencesUseCase
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@OptIn(FlowPreview::class)
+internal class SourcesViewModel @Inject constructor(
+    private val getNewsSourcesUseCase: GetNewsSourcesUseCase,
+    private val userPreferencesUseCase: UserPreferencesUseCase,
+    private val uiSourceMapper: UiSourceMapper,
+) : ViewModel() {
+
+    val uiState: StateFlow<UiState> = userPreferencesUseCase.language
+        .flatMapConcat { language ->
+            combine(
+                getNewsSourcesUseCase.execute(param = SourcesQueryParam(language = language)),
+                userPreferencesUseCase.followedSources,
+            ) { eitherNewsSources, followedSources ->
+                eitherNewsSources.fold(
+                    ifLeft = { error ->
+                        UiState(loading = false, error = error.message)
+                    },
+                    ifRight = { sources ->
+                        UiState(
+                            loading = false,
+                            sources = uiSourceMapper.from(
+                                input = sources,
+                                followedChecker = { sourceId ->
+                                    followedSources.any { it.id == sourceId }
+                                }
+                            )
+                        )
+                    }
+                )
+            }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = UiState(loading = true),
+            started = SharingStarted.WhileSubscribed()
+        )
+}
